f6415ced53a5500c8d9a3c4b15ac1bc490baa972
Bruce0203 <BruceFreedy@gmail.com>
2023-05-26 17:14:09 +0900

    chore: publish

--- build-logic/src/main/kotlin/minestom.publishing-conventions.gradle.kts
+++ build-logic/src/main/kotlin/minestom.publishing-conventions.gradle.kts
@@ -4,11 +4,19 @@ plugins {
     id("net.kyori.indra.publishing.sonatype")
 }
 
-indraSonatype {
-    useAlternateSonatypeOSSHost("https://s01.oss.sonatype.org/content/repositories/releases/")
-}
-
 indra {
+    repositories {
+        maven {
+            name = "pkgs"
+            url = uri("https://s01.oss.sonatype.org/content/repositories/releases/")
+            credentials {
+
+                username = System.getenv("SONATYPE_USERNAME")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
+        }
+    }
+
     javaVersions {
         target(17)
         testWith(17)
