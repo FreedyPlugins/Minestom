dc1c0b4ff9514542350383b6575d770a00d368c7
Bruce0203 <BruceFreedy@gmail.com>
2023-05-26 17:25:59 +0900

    chore: publish

--- build-logic/src/main/kotlin/minestom.publishing-conventions.gradle.kts
+++ build-logic/src/main/kotlin/minestom.publishing-conventions.gradle.kts
@@ -1,23 +1,15 @@
-import net.kyori.indra.IndraExtension
-
 plugins {
     id("net.kyori.indra")
     id("net.kyori.indra.publishing")
     id("net.kyori.indra.publishing.sonatype")
 }
 
-fun IndraExtension.setupCredential() {
-    val SONATYPE_USERNAME: String? by System.getenv()
-    val SONATYPE_PASSWORD: String? by System.getenv()
-    publishReleasesTo(
-            SONATYPE_USERNAME ?: return,
-            SONATYPE_PASSWORD ?: return
-    )
-}
-
 indra {
-    setupCredential()
-
+    runCatching {
+        val SONATYPE_USERNAME: String by System.getenv()
+        val SONATYPE_PASSWORD: String by System.getenv()
+        publishReleasesTo(SONATYPE_USERNAME, SONATYPE_PASSWORD)
+    }
     javaVersions {
         target(17)
         testWith(17)
